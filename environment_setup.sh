42a8cd166ceceb84ec90f91751b139d75222913b
Quanghihicoder <hoangquang2508@gmail.com>
2025-06-13 10:05:07 +1000

    environment setup

---
+++ environment_setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Set up backend environment
+cd backend
+cp .env.template .env
+cd ..
+
+# Set up frontend environment
+cd frontend
+cp .env.template .env
+cd ..
+
+# Set up worker/tiling_image environment
+cd worker/tiling_image
+cp .env.template .env
+cd ../../
+
+echo ".env files copied successfully."
