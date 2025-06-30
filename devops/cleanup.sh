ee5708cdffccb7e71fe9095ff78063c489515a2d
Quanghihicoder <hoangquang2508@gmail.com>
2025-06-30 16:27:21 +1000

    change script

--- devops/cleanup.sh
+++ devops/cleanup.sh
@@ -6,4 +6,5 @@ aws s3 rb s3://tilelens-terraform --force --region ap-southeast-2
 # Delete terraform lock table
 aws dynamodb delete-table \
   --table-name tilelens-terraform-lock \
-  --region ap-southeast-2
+  --region ap-southeast-2 \
+  --no-cli-pager
--- devops/init.sh
+++ devops/init.sh
@@ -9,4 +9,5 @@ aws dynamodb create-table \
   --attribute-definitions AttributeName=LockID,AttributeType=S \
   --key-schema AttributeName=LockID,KeyType=HASH \
   --billing-mode PAY_PER_REQUEST \
-  --region ap-southeast-2
+  --region ap-southeast-2 \
+  --no-cli-pager
