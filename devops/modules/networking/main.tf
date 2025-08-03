05f62fa6e3a084564cabde55382b38ecfc0002ce
Quanghihicoder <hoangquang2508@gmail.com>
2025-08-03 21:49:49 +1000

    add nat gateway for ecs in private subnets

--- devops/modules/networking/main.tf
+++ devops/modules/networking/main.tf
@@ -89,3 +89,50 @@ resource "aws_db_subnet_group" "rds_subnet_group" {
   name       = "${var.project_name}-rds-subnet-group"
   subnet_ids = [aws_subnet.private_a.id, aws_subnet.private_b.id]
 }
+
+
+# NAT Gateway for ECS Fargate on private subnet to pull ECR image
+resource "aws_eip" "nat" {
+  domain = "vpc"
+  tags = {
+    Name = "${var.project_name}-nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "service_nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_a.id 
+
+  tags = {
+    Name = "${var.project_name}-nat-gateway"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.service_nat.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private_assoc_a" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_assoc_b" {
+  subnet_id      = aws_subnet.private_b.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route" "ecr_access" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "3.104.83.140/32"  # ECR API endpoint in ap-southeast-2
+  nat_gateway_id         = aws_nat_gateway.service_nat.id
+}
